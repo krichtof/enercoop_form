0cae398b839b2508af68e840eb38863bded498f7
Christophe Robillard <geek@robiweb.net>
2018-07-09 23:39:39 +0200

    checks uniqueness of email

--- app/forms/user_step_one_form.rb
+++ app/forms/user_step_one_form.rb
@@ -12,4 +12,17 @@ class UserStepOneForm < Rectify::Form
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates_presence_of :first_name, :last_name, :street_name, :zip_code, :city, :email, :password
+
+  validate :validate_email_uniqueness
+
+  private
+
+    def validate_email_uniqueness
+      return if unique_email?
+      errors.add(:email, 'already in use')
+    end
+
+    def unique_email?
+      User.where('lower(email) = lower(?)', email).empty?
+    end
 end
